c6f274273eff1f39ba0ddf7a4d4271d83f0591c2
amir2227 <amir.reza2823@gmail.com>
2021-07-20 19:18:43 +0430

    add some motion

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ const emailCheck = () => {
     document.getElementById('emailWarning').classList.remove('hidden');
   }
 }
+
+
+// Scroll Animation
+const sr = ScrollReveal({
+  origin: 'top',
+  distance:'85px',
+  duration:2000,
+  reset: true
+})
+
+sr.reveal ('.h3_delay',{delay:600})
+sr.reveal ('.h5_delay',{delay:500})
+sr.reveal ('.customer__logo',{delay:500})
+sr.reveal ('.footer__list li',{})
+sr.reveal ('.footer__social li',{})
+
+sr.reveal ('.features__box__image',{})
+sr.reveal ('.img_delay',{})
+sr.reveal ('.contact__form div',{delay:200})
+sr.reveal ('input',{})
+sr.reveal ('textarea',{})
